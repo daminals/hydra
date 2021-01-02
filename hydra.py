0004edc0d801b77f37abb8b8a8cfb3661893e1d8
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-01-02 13:12:16 -0500

    hydra1

---
+++ hydra.py
@@ -0,0 +1,21 @@
+# hydra.py
+
+import tkinter as tk
+
+class Hydra():
+	
+	def __init__(self):
+		Window = self.Window = tk.Tk()
+		Window.title("Hydra Virus")
+		text = tk.Label(Window, text='Cut off a head, two more grow in its place \n\n\n\n\n\n\n\n')
+		text.pack()
+		def on_close():
+			self.Window.destroy()
+			for i in range(2):
+				a = Hydra()
+
+		Window.protocol("WM_DELETE_WINDOW", on_close)
+
+
+hydra = Hydra()
+hydra.Window.mainloop()
